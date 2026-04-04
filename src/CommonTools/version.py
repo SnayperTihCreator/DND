35e173a7f94d4719923dc8e95ac14cc642a5daa3
SnayperTihCreator <snajpertih@gmail.com>
2026-04-04 17:09:02 +0300

    fix:version

--- src/CommonTools/version.py
+++ src/CommonTools/version.py
@@ -1,1 +1,6 @@
-__version__ = "1.1.0b3"
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("dnd")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "1.1.0b2"
